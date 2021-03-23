420cd05ff45ffdadd9b56f695e2156ab67988ec5
Mahrkeenerh <samuelbuban@gmail.com>
2021-03-23 23:09:14 +0100

    Testing

--- PageUpdateBot.py
+++ PageUpdateBot.py
@@ -2,14 +2,16 @@ import requests, winsound, webbrowser, json
 from time import sleep
 
 with open('config.txt') as file:
-    url = json.load(file)['url']
+    json_data = json.load(file)
+    url = json_data['url']
+    frequency = json_data['update_frequency']
 
 old_request = requests.get(url=url)
 new_request = old_request
 
 # while old_request.content == new_request.content:
 while True:
-    sleep(5)
+    sleep(frequency)
     new_request = requests.get(url=url)
     if new_request.status_code != 200:
         print(new_request.status_code)
---
+++ README.md
@@ -0,0 +1,3 @@
+# PAGE UPDATE BOT
+
+It's a program, that will load the specified url once it's changed.
